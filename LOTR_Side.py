6218587b3c7da66f369760081310db668d22a748
espinozatj <tadeo.espinoza@slu.edu>
2024-04-18 15:19:58 -0500

    Edited

--- LOTR_Side.py
+++ LOTR_Side.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+characters_of_the_ring = pd.read_csv("lotr_characters.csv")
+script_of_the_ring = pd.read_csv("lotr_scripts.csv")
+
+print(characters_of_the_ring.head())
